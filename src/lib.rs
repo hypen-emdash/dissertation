d81e362e215041e62898a6732b6628f59d7d51cb
hypen-emdash <joffe.daniel@gmail.com>
2021-02-20 12:02:30 +0000

    Create blank infrastructure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,22 @@
-// TODO
+use petgraph::matrix_graph::MatrixGraph;
+use petgraph::Undirected;
+
+pub type GuestID = usize;
+pub type Relationship = i64;
+
+pub type Plan = Vec<Vec<GuestID>>;
+
+pub type Relationships = MatrixGraph<GuestID, Relationship, Undirected>;
+
+pub trait SeatingPlanner {
+    fn plan(&self, relationships: Relationships, n_tables: usize) -> Plan;
+}
+
+pub struct Score {
+    pub n_lonely: usize,
+    pub happiness: Relationship,
+}
+
+pub fn quality(plan: Plan, relationships: Relationships) -> Score {
+    todo!()
+}
