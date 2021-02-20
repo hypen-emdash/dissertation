67e76becdea06be7950294cf339a8adb0edddabf
hypen-emdash <joffe.daniel@gmail.com>
2021-02-20 12:08:31 +0000

    Separate out two different metrics.
    We don't always care about both, and one is more expensive to compute.

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,10 @@ pub trait SeatingPlanner {
     fn plan(&self, relationships: Relationships, n_tables: usize) -> Plan;
 }
 
-pub struct Score {
-    pub n_lonely: usize,
-    pub happiness: Relationship,
+pub fn lonely_guests(plan: Plan, relationships: Relationships) -> usize {
+    todo!()
 }
 
-pub fn quality(plan: Plan, relationships: Relationships) -> Score {
+pub fn total_happiness(plan: Plan, relationships: Relationships) -> Relationship {
     todo!()
 }
