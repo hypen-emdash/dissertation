ae554b2efbc7515789996edcb13428a68741351b
hypen-emdash <joffe.daniel@gmail.com>
2021-02-23 09:16:48 +0000

    Implement second metric.
    Also a few naming changes.

--- src/lib.rs
+++ src/lib.rs
@@ -40,10 +40,10 @@ pub trait SeatingPlanner {
 pub fn lonely_guests(plan: &Plan, relationships: &GuestRelations) -> usize {
     let mut n_lonely = 0;
     for table in plan {
-        for g1 in table.iter() {
+        for guest1 in table {
             // A guest is lonely if they have a neutral or negative relationship with everyone else at the table.
             // It should be assumed that everyone has a neutral relationship with themself.
-            if table.iter().all(|g2| relationships.relationship(*g1, *g2) <= 0) {
+            if table.iter().all(|guest2| relationships.relationship(*guest1, *guest2) <= 0) {
                 n_lonely += 1;
             }
         }
@@ -53,5 +53,14 @@ pub fn lonely_guests(plan: &Plan, relationships: &GuestRelations) -> usize {
 }
 
 pub fn total_happiness(plan: &Plan, relationships: &GuestRelations) -> i64 {
-    todo!()
+    let mut total = 0;
+    for table in plan {
+        for guest1 in table {
+            for guest2 in table {
+                total += relationships.relationship(*guest1, *guest2)
+            }
+        }
+    }
+
+    total
 }
