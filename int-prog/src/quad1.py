50155f2d5bf05cc26899d25e2d4693e4829031c8
hypen-emdash <joffe.daniel@gmail.com>
2021-03-09 09:13:09 +0000

    Add demonstration of why quadratics don't work.

---
+++ int-prog/src/quad1.py
@@ -0,0 +1,21 @@
+from ortools.linear_solver import pywraplp
+
+solver = pywraplp.Solver.CreateSolver("SCIP")
+inf = solver.infinity()
+
+x = solver.IntVar(0.0, inf, "x")
+y = solver.IntVar(0.0, inf, "y")
+prod = solver.IntVar(0.0, inf, "x*y")
+
+solver.Add(x <= 10)
+solver.Add(y <= 10)
+solver.Add(prod == x * y)
+
+solver.Maximize(prod)
+
+status = solver.Solve()
+
+if status == pywraplp.Solver.OPTIMAL:
+    print(f"obj val = {solver.Objective().Value()}") 
+    print(f"x = {x.solution_value()}")
+    print(f"y = {y.solution_value()}")
---
+++ int-prog/src/quad2.py
@@ -0,0 +1,20 @@
+from ortools.linear_solver import pywraplp
+
+solver = pywraplp.Solver.CreateSolver("SCIP")
+inf = solver.infinity()
+
+x = solver.IntVar(0.0, inf, "x")
+y = solver.IntVar(0.0, inf, "y")
+prod = solver.IntVar(0.0, inf, "x*y")
+
+solver.Add(x <= 10)
+solver.Add(y <= 10)
+
+solver.Maximize(x * y)
+
+status = solver.Solve()
+
+if status == pywraplp.Solver.OPTIMAL:
+    print(f"obj val = {solver.Objective().Value()}") 
+    print(f"x = {x.solution_value()}")
+    print(f"y = {y.solution_value()}")
