978817c47937b27f5b261d13f2136e638ada664d
hypen-emdash <joffe.daniel@gmail.com>
2021-03-10 15:39:37 +0000

    Wrote logic for friends-of-friends.
    Even when it runs, it may not generate new friendships.

--- src/bin/data-gen.rs
+++ src/bin/data-gen.rs
@@ -12,11 +12,25 @@ struct Problem {
 }
 
 fn main() {
-    let friend_lists = create_friend_lists(1000);
+    let mut friend_lists = create_friend_lists(16);
     for (i, list) in friend_lists.iter().enumerate() {
         println!("{}: {:?}", i, list);
     }
-    println!("avg pop: {}", friend_lists.iter().map(|list| list.len()).sum::<usize>() as f64 / friend_lists.len() as f64);
+    println!(
+        "avg pop: {}",
+        friend_lists.iter().map(|list| list.len()).sum::<usize>() as f64
+            / friend_lists.len() as f64
+    );
+
+    friends_of_friends(&mut friend_lists);
+    for (i, list) in friend_lists.iter().enumerate() {
+        println!("{}: {:?}", i, list);
+    }
+    println!(
+        "avg pop: {}",
+        friend_lists.iter().map(|list| list.len()).sum::<usize>() as f64
+            / friend_lists.len() as f64
+    );
 }
 
 fn create_relations(n_guests: usize) -> GuestRelations {
@@ -44,6 +58,30 @@ fn create_friend_lists(n_guests: usize) -> Vec<Vec<usize>> {
     friend_lists
 }
 
+fn friends_of_friends(friend_lists: &mut [Vec<usize>]) {
+    let mut rng = thread_rng();
+
+    for i in 0..friend_lists.len() {
+        // A random number of times, but with decreasing probability.
+        // Not guaranteed to run even once.
+        while rng.gen_range(0..=friend_lists[i].len()) == 0 {
+            let mutual_friend = friend_lists[i]
+                .choose(&mut rng)
+                .copied()
+                .expect("Everyone should have at least one friend by now.");
+
+            // Find a friend of our mutual friend, and make them friends with the current guest.
+            let new_friend = friend_lists[mutual_friend]
+                .choose(&mut rng)
+                .copied()
+                .expect("Everyone should have at least one friend by now.");
+            
+            friend_lists[i].push(new_friend);
+            friend_lists[new_friend].push(i);
+        }
+    }
+}
+
 fn random_associate<R>(mut rng: R, person: usize, choices: Range<usize>) -> usize
 where
     R: Rng,
@@ -58,10 +96,14 @@ where
 
 fn fill_adj_matrix(lists: &[Vec<usize>], val: i64, matrix: &mut [Vec<i64>]) {
     for (guest_id, rel_list) in lists.iter().enumerate() {
-        for rel_id in rel_list {
-            // The adjacency list should be symmetrical, so we don't need to
-            // reflect this.
-            matrix[guest_id][*rel_id] = val;
+        for &rel_id in rel_list {
+            if rel_id != guest_id {
+                // No-one is considered a relative of themselves.
+
+                // The adjacency list should be symmetrical, so we don't need to
+                // reflect this.
+                matrix[guest_id][rel_id] = val;
+            }
         }
     }
 }
