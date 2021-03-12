0de4539a11a84f38cad8e53084836c7d89e7e881
hypen-emdash <joffe.daniel@gmail.com>
2021-03-12 16:29:34 +0000

    Bare-bones incorrect implementation.
    Barely any protocol. Also segfaults.

--- int-prog/src/main.py
+++ int-prog/src/main.py
@@ -1,4 +1,23 @@
+from dataclasses import dataclass
 from ortools.linear_solver import pywraplp
+
+
+@dataclass
+class Problem:
+    guest_relations: int
+    n_tables: int
+
+
+class Solution:
+    def __init__(self, status, variables):
+        self.status = status
+        self.variables = variables
+
+
+def main():
+    problem = get_problem()
+    solution = plan_dinner(problem.guest_relations, problem.n_tables)
+    display_sol(solution)
 
 
 def plan_dinner(guest_relations, n_tables):
@@ -11,13 +30,75 @@ def plan_dinner(guest_relations, n_tables):
     * n_tables: the number of tables. Should evenly divide the number of guests.
     """
 
-    assert len(
-        guest_relations % n_tables == 0, "Partial filling of tables not yet supported."
-    )
+    assert (
+        len(guest_relations) % n_tables == 0
+    ), "Partial filling of tables not yet supported."
 
     solver = pywraplp.Solver.CreateSolver("SCIP")
 
     n_guests = len(guest_relations)
     table_size = int(n_guests / n_tables)
 
-    assert False, "Not yet implemented."
+    variables = Variables(solver, n_guests, n_tables)
+    add_constraints(solver, variables)
+    solver.Maximize(get_objective(guest_relations, variables))
+
+    status = solver.Solve()
+
+    return Solution(status, variables)
+
+
+class Variables:
+    def __init__(self, solver, n_guests, n_tables):
+        # indexed [table][guest]
+        self.at_table = [
+            [solver.IntVar(0, 1, f"at_table[{i}{j}]") for j in range(n_guests)]
+            for i in range(n_tables)
+        ]
+
+        # indexed [table][guest1][guest2]
+        self.pair_at_table = [
+            [
+                [
+                    solver.IntVar(0, 1, f"pair_at_table[{i}][{j}][{k}]")
+                    for k in range(n_guests)
+                ]
+                for j in range(n_guests)
+            ]
+            for i in range(n_tables)
+        ]
+
+
+def get_objective(guest_relations, variables):
+    # TODO: implement
+    return variables.pair_at_table[0][0][0]
+
+
+def add_constraints(solver, variables):
+    # TODO: implement
+    pass
+
+
+def get_problem():
+    return Problem(
+        [
+            [0, 1, 1, 1, 0, 0, 0, 0],
+            [1, 0, 1, 1, 0, 0, 0, 0],
+            [1, 1, 0, 1, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 1, 0, 1, 1],
+            [0, 0, 0, 0, 1, 1, 0, 1],
+            [0, 0, 0, 0, 1, 1, 1, 0],
+        ],
+        2,
+    )
+
+
+def display_sol(solution):
+    if solution.status in [pywraplp.Solver.OPTIMAL, pywraplp.Solver.FEASIBLE]:
+        print(solution.variables.at_table)
+
+
+if __name__ == "__main__":
+    main()
