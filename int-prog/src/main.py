7ee3da6c0641e4b6aff415504dfaa74262ca1255
hypen-emdash <joffe.daniel@gmail.com>
2021-03-12 15:26:18 +0000

    Restart implementation of integer programming.

--- int-prog/src/main.py
+++ int-prog/src/main.py
@@ -20,19 +20,4 @@ def plan_dinner(guest_relations, n_tables):
     n_guests = len(guest_relations)
     table_size = int(n_guests / n_tables)
 
-    # For now, our answer is represented as an graph with edges representing "sits next to".
-    # There are of course some restrictions on the structure of this graph, eg complete
-    # components of limited size.
-
-    # `seated[i][j]` is 1 if guest j is seated at table i, else 0.
-    seated = [
-        [solver.IntVar(0, 1, f"seated[{t}][{g}]") for g in range(n_guests)]
-        for t in range(n_tables)
-    ]
-
-    # `adjacent[i][j]`` is 1 if guests i and j are seated at the same table, else 0.
-    # This will have to be controlled by a constraint.
-    adjacent = [
-        [solver.IntVar(0, 1, f"adjacent[{i}][{j}]") for i in range(n_guests)]
-        for j in range(n_guests)
-    ]
+    assert False, "Not yet implemented."
