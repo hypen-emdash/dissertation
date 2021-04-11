dfb846a9f3496867154ced848572676f3b56ee9b
hypen-emdash <joffe.daniel@gmail.com>
2021-04-11 22:04:05 +0100

    Remove old implementations.

--- src/metrics.rs
+++ src/metrics.rs
@@ -69,34 +69,3 @@ impl Metrics {
             .expect("Expected a nonempty Metrics object.")
     }
 }
-
-pub fn lonely_guests(plan: &Plan, relationships: &GuestRelations) -> usize {
-    let mut n_lonely = 0;
-    for table in plan {
-        for guest1 in table {
-            // A guest is lonely if they have a neutral or negative relationship with everyone else at the table.
-            // It should be assumed that everyone has a neutral relationship with themself.
-            if table
-                .iter()
-                .all(|guest2| relationships.relationship(*guest1, *guest2) <= 0)
-            {
-                n_lonely += 1;
-            }
-        }
-    }
-
-    n_lonely
-}
-
-pub fn total_happiness(plan: &Plan, relationships: &GuestRelations) -> i64 {
-    let mut total = 0;
-    for table in plan {
-        for guest1 in table {
-            for guest2 in table {
-                total += relationships.relationship(*guest1, *guest2)
-            }
-        }
-    }
-
-    total
-}
