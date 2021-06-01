9e9e4e80ef43407855e5d70f2c28f6497cbcf138
hypen-emdash <joffe.daniel@gmail.com>
2021-06-01 18:50:13 +0100

    Significance everything against everything.

--- py/src/sig.py
+++ py/src/sig.py
@@ -2,22 +2,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats
 
+METRIC_COLUMNS = [
+    "total_happiness",
+    "mean_happiness",
+    "median_happiness",
+    "min_happiness",
+    "max_happiness",
+    "n_lonely",
+    "seconds",
+]
+
+SOLVERS = ["hill-solve", "int-prog", "lahc-solve"]
+SUITES = ["complete-suite", "ring-suite", "rand-suite", "tense-suite"]
+
 def main():
-    path1 = "./weddings/lahc-solve_complete-suite.csv"
-    path2 = "./weddings/hill-solve_complete-suite.csv"
+    lows = []
+    highs = []
+    for suite in SUITES:
+        for solver1 in SOLVERS:
+            for solver2 in SOLVERS:
+                if solver1 < solver2:
+                    df1 = pd.read_csv(get_csv_path(solver1, suite))
+                    df2 = pd.read_csv(get_csv_path(solver2, suite))
+                    for col in METRIC_COLUMNS:
+                        p = scipy.stats.mannwhitneyu(df1[col], df2[col]).pvalue
+                        if p < 0.05:
+                            lows.append((suite, solver1, solver2, col, p))
+                        else:
+                            highs.append((suite, solver1, solver2, col, p))
+    
+    print("SIGNIFICANT RESULTS:")
+    for l in lows:
+        print(l)
+    print("----------------------------------------")
+    print("INSIGNIFICANT RESULTS:")
+    for h in highs:
+        print(h)
+    
+    print(f"n sig: {len(lows)}")
+    print(f"n nsig: {len(highs)}")
 
-    df1 = pd.read_csv(path1)
-    df2 = pd.read_csv(path2)
+    
 
-    n1 = df1["n_people"]
-    n2 = df2["n_people"]
-
-    print(scipy.stats.mannwhitneyu(n1, n2))
-
-    t1 = df1["seconds"]
-    t2 = df2["seconds"]
-
-    print(scipy.stats.mannwhitneyu(t1, t2))
+def get_csv_path(solver, suite):
+    return f"weddings/{solver}_{suite}.csv"
 
 if __name__ == "__main__":
     main()
